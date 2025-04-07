cf26e09b4fd15ba171e23c7b63373b2f7c7b73df
Rustambek <rusik.khanov06@gmail.com>
2025-04-07 21:35:53 +0500

    Grid added

--- warm-up-tasks/tic-tac-toe/main.py
+++ warm-up-tasks/tic-tac-toe/main.py
@@ -8,7 +8,7 @@ RED = (255, 0, 0)
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((900, 900))
 pygame.display.set_caption("Tic Tac Toe")
 
 clock = pygame.time.Clock()
@@ -20,6 +20,12 @@ while running:
             running = False
 
     screen.fill(WHITE)
+
+    pygame.draw.line(screen, RED, (300, 100), (300, 900), 5)
+    pygame.draw.line(screen, RED, (600, 100), (600, 900), 5)
+
+    pygame.draw.line(screen, RED, (0, 367), (900, 367), 5)
+    pygame.draw.line(screen, RED, (0, 634), (900, 634), 5)
 
     pygame.display.flip()
     clock.tick(60)
