f44918dc3926f50bfe18134bd133917f98317167
Rustambek <rusik.khanov06@gmail.com>
2025-04-10 12:38:35 +0500

    Fixed noughts and crosses drawing

--- warm-up-tasks/tic-tac-toe/main.py
+++ warm-up-tasks/tic-tac-toe/main.py
@@ -1,43 +1,29 @@
 import pygame
 
-grid = [None] * 9
+cells = [[None, None, None] for _ in range(9)]
 
 def drawGrid():
-    pygame.draw.line(screen, RED, (0, 100), (0, 900), 5)
-    pygame.draw.line(screen, RED, (900, 100), (900, 900), 5)
-
-    pygame.draw.line(screen, RED, (0, 100), (900, 100), 5)
-    pygame.draw.line(screen, RED, (0, 900), (900, 900), 5)
-
+    # Вертикальные линии
     pygame.draw.line(screen, RED, (300, 100), (300, 900), 5)
     pygame.draw.line(screen, RED, (600, 100), (600, 900), 5)
 
+    # Горизонтальные линии
+
+    pygame.draw.line(screen, RED, (0, 100), (900, 100), 5)
     pygame.draw.line(screen, RED, (0, 367), (900, 367), 5)
     pygame.draw.line(screen, RED, (0, 634), (900, 634), 5)
 
-def checkPosition(pos):
-    if pos[0] > 0 and pos[0] < 267 and pos[1] > 100 and pos[1] < 367:
-        return [True, 0, 100]
-    if pos[0] > 267 and pos[0] < 534 and pos[1] > 100 and pos[1] < 367:
-        return [True, 267, 100]
-    if pos[0] > 534 and pos[0] < 900 and pos[1] > 100 and pos[1] < 367:
-        return [True, 534, 100]
-    
-    if pos[0] > 0 and pos[0] < 267 and pos[1] > 367 and pos[1] < 634:
-        return [True, 0, 367]
-    if pos[0] > 267 and pos[0] < 534 and pos[1] > 367 and pos[1] < 634:
-        return [True, 267, 367]
-    if pos[0] > 534 and pos[0] < 900 and pos[1] > 367 and pos[1] < 634:
-        return [True, 534, 367]
+def checkPosition(pos, move):
+    for i in range(3):
+        for j in range(3):
+            if (pos[0] > i * 300) and (pos[0] < (i + 1) * 300) and (pos[1] > 100 + j * 267) and (pos[1] < 100 + (j + 1) * 267):
+                if move is not None:  # Проверка на допустимость движения
+                    cells[i + j * 3][0] = move  # Записываем состояние ячейки
+                cells[i + j * 3][1] = i * 300  # x координата
+                cells[i + j * 3][2] = 100 + j * 267  # y координата
+                return True
+    return False
 
-    if pos[0] > 0 and pos[0] < 267 and pos[1] > 634 and pos[1] < 900:
-        return [True, 0, 534]
-    if pos[0] > 267 and pos[0] < 534 and pos[1] > 634 and pos[1] < 900:
-        return [True, 267, 534]
-    if pos[0] > 534 and pos[0] < 900 and pos[1] > 634 and pos[1] < 900:
-        return [True, 534,534]
-
-    return [False, None, None]
 
 def drawCross(pos_0, pos_1):
     pygame.draw.line(screen, RED, (pos_0 + 25, pos_1 + 25), (pos_0 + 242, pos_1 + 242), 10)
@@ -50,12 +36,12 @@ def playerMove(move, isValid):
     
     if move == True and isValid == True:
         print("X")
-        return [False, True]
+        return False
     elif move == False and isValid == True:
         print("O")
-        return [True, False]
+        return True
     else:
-        return [move, None]
+        return move
         
 # Constants
 WHITE = (255,255,255)
@@ -73,8 +59,6 @@ pygame.display.set_caption("Tic Tac Toe")
 clock = pygame.time.Clock()
 running = True
 
-isCross = False
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -83,18 +67,18 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
 
-            valid, pos_0, pos_1 = checkPosition(pos)
-            player, isCross = playerMove(player, valid)
+            valid = checkPosition(pos, player)
+            player = playerMove(player, valid)
 
     screen.fill(WHITE)
 
     drawGrid()
-    if isCross:
-        drawCross(pos_0, pos_1)
-    elif isCross == None:
-        pass
-    else:
-        drawCircle(pos_0, pos_1)
+
+    for cell in cells:
+        if cell[0]:
+            drawCross(cell[1], cell[2])
+        elif cell[0] == False:
+            drawCircle(cell[1], cell[2])
 
     pygame.display.flip()
     clock.tick(60)
