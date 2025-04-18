0682ec9ce2443388265be4aad4675a995436c29a
Rustambek <rusik.khanov06@gmail.com>
2025-04-19 01:18:49 +0500

    Random Number Generator created

--- RandomNumber/server/index.js
+++ RandomNumber/server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 app.use(cors())
 
 app.get('/', (req, res) => {
-      res.send('Hello from our server!')
+      res.send(Math.floor(Math.random() * 1000) + 1)
 })
 
 app.listen(8080, () => {
